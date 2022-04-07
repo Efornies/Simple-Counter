96bf569f945c43d4d5c1e7ab6aedd4c4ceed1bcf
Efornies <eleforbel@gmail.com>
2022-04-07 10:58:30 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,16 +2,30 @@ import React, { useState, useEffect } from "react";
 import SecondCounter from "./secondcounter.jsx";
 
 const Home = () => {
+	const time = <i className="fas fa-clock"></i>;
 	const [timer, setTimer] = useState(0);
+
 	useEffect(() => {
 		const interval = setInterval(() => {
 			setTimer((timer) => timer + 1);
 		}, 1000);
+
 		return () => clearInterval(interval);
-	}, []);
+	});
+
 	return (
-		<div className="row">
-			<SecondCounter seconds={timer} />
+		<div className="container justify-content-center">
+			<div className="container shadow-sm bg-dark">
+				<div className="row justify-content-center">
+					<SecondCounter seconds={time} />
+					<SecondCounter num={Math.floor((timer / 100000) % 10)} />
+					<SecondCounter num={Math.floor((timer / 10000) % 10)} />
+					<SecondCounter num={Math.floor((timer / 1000) % 10)} />
+					<SecondCounter num={Math.floor((timer / 100) % 10)} />
+					<SecondCounter num={Math.floor((timer / 10) % 10)} />
+					<SecondCounter num={Math.floor((timer / 1) % 10)} />
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/js/component/secondcounter.jsx
+++ src/js/component/secondcounter.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const SecondCounter = (props) => {
 	return (
-		<div className="row">
-			<div
-				style={{ width: "2rem", height: "2.5rem" }}
-				className="bg-dark text-white m-2">
-				{props.seconds}
+		<div className="col-sm">
+			<div className="card bg-light">
+				<div className="card-body d-flex justify-content-center ">
+					<h5 className="card-title ">
+						{props.seconds} {props.num}
+					</h5>
+				</div>
 			</div>
 		</div>
 	);
+};
+
+SecondCounter.propTypes = {
+	num: PropTypes.number,
+	seconds: PropTypes.object,
 };
 
 export default SecondCounter;
